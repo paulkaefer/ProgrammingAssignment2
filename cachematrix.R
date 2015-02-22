266b353dfb71e885e049791fb45cb3e3fcb5707e
Paul Kaefer <paul.kaefer@marquette.edu>
2015-02-22 15:41:21 -0600

    added initial comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+##
+## Functions that create a matrix object with a cacheable inverse value.
+## In other words, rather than having to re-compute the inverse every time it is needed,
+##  the value is stored in the object.
+##
+## Forked from https://github.com/rdpeng/ProgrammingAssignment2 for the R Programming 
+##  online course (https://class.coursera.org/rprog-011).
+##
+## Paul Kaefer, February 2015
 
 ## Write a short comment describing this function
 
